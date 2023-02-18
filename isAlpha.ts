e20be0b729225f8c6df0c564c573a935cdbb5039
rem <nthuyen6797@gmail.com>
2023-02-19 02:09:46 +0700

    isAlpha

---
+++ isAlpha.ts
@@ -0,0 +1,15 @@
+function isAlpha(input: string): boolean {
+    // regex that will match any string that only has upper and lowercase letters 
+    const alphaRegex = /^[a-z]*$/i
+    // test our input using the regex
+    const result = alphaRegex.test(input);
+    return result
+}
+console.log(`isAlpha("War and Peace"):`);
+console.log(isAlpha("War and Peace"));
+
+console.log(`isAlpha("Atonement"):`);
+console.log(isAlpha("Atonement"));
+
+console.log(`isAlpha("1Q84"):`);
+console.log(isAlpha("1Q84"));
