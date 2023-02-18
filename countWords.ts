9ee717934f9a4e3e77c934e60caa20693e48af52
rem <nthuyen6797@gmail.com>
2023-02-19 00:34:36 +0700

    countWords

---
+++ countWords.ts
@@ -0,0 +1,16 @@
+function countWords(input: string) {
+    // split the words using a regex that will match any occurence of
+    // a space, underscore or dash characters
+    const words = input.split(/[ _-]/);
+    // return the lenght of the array that is the result of the split
+    return words.length;
+}
+
+console.log(`countWords("War and Peace"):`);
+console.log(countWords("War and Peace"));
+
+console.log(`countWords("catcher-in-the-rye"):`);
+console.log(countWords("catcher-in-the-rye"));
+
+console.log(`countWords("for_whom the-bell-tolls"):`);
+console.log(countWords("for_whom the-bell-tolls"));
