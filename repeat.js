317aa6b9ef7b18b49841e1ad728aa9033196e6c5
rem <nthuyen6797@gmail.com>
2023-02-19 01:46:04 +0700

    repeat

---
+++ repeat.js
@@ -0,0 +1,15 @@
+"use strict";
+function repeat(input, times) {
+    // create a new array that with length of `times`
+    // and set each element to the value of the `input` string
+    const instances = new Array(times).fill(input);
+    // join the elements of the array together
+    const result = instances.join("");
+    return result;
+}
+console.log(`repeat("War", 3 ):`);
+console.log(repeat("War", 3));
+console.log(`repeat("rye", 1):`);
+console.log(repeat("rye", 1));
+console.log(`repeat("bell", 0):`);
+console.log(repeat("bell", 0));
